0a73e6b2475cbbc31f2930dca555c545e82f09da
Per Johansson <per.johansson@post.com>
2018-03-03 20:28:53 +0000

    initial setup

---
+++ src/main/java/com/headstartech/vas/vas/VasApplication.java
@@ -0,0 +1,12 @@
+package com.headstartech.vas.vas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasApplication.class, args);
+	}
+}
